9e8636b1ab5bf71ecdb4c31a18c597b2e45b0d8a
jonathan <112445400+ak9mee@users.noreply.github.com>
2023-05-08 18:23:33 -0500

    Create agent.lua

---
+++ dahood/agent.lua
@@ -0,0 +1,30 @@
+       getgenv().Invite = 'wPQHasG3nX'
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Discord"))()
+--[[if FLUXUS_LOADED then
+    getgenv().Invite = 'wPQHasG3nX'
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Discord"))()
+elseif KRNL_LOADED then
+       getgenv().Invite = 'wPQHasG3nX'
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Discord"))()
+else
+    local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Random.lua"))()
+
+    -- Creates an interactable prompt
+    Inviter.Prompt({
+        name = "Agent Revamped",
+        invite = "https://discord.gg/wPQHasG3nX",
+        script = "https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits",
+    })
+    -- thank you using a good sploit
+end]]
+--// Old Shitty Dog Versions
+
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+--[[if syn then
+loadstring(game:HttpGet(Versions..'GoodSploits'))()
+else
+loadstring(game:HttpGet(Versions..'BadSploits'))()
+end]]
